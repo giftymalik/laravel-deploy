307790ba285e32121e2fc3b243dfbcfb6573a3c1
Sumit Malik <sumit.malik@meltag.com>
2015-05-20 19:09:03 +0530

    Add ready-server macro

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -1,7 +1,38 @@
 @servers(['web-1' => 'staging:one', 'web-2' => 'staging:two'])
 
-<?php $what = 'Okay? Deployed!'; ?>
+<?php $what = 'Okay? Deploying!'; ?>
 
-@task('deploy', ['on' => ['web-1', 'web-2'], 'parallel' => true])
+@macro('ready-server')
+    test
+    update
+    server-setup
+    hhvm
+    composer
+@endmacro
+
+@task('test', ['on' => ['web-1', 'web-2'], 'parallel' => true])
     echo {{ $what }}
+@endtask
+
+@task('update', ['on' => ['web-1', 'web-2'], 'parallel' => true])
+    sudo apt-get update
+@endtask
+
+@task('server-setup', ['on' => ['web-1', 'web-2'], 'parallel' => true, 'confirm' => true])
+    sudo apt-get install nginx mysql-client git curl software-properties-common
+@endtask
+
+@task('hhvm', ['on' => ['web-1', 'web-2'], 'parallel' => true, 'confirm' => true])
+    sudo apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0x5a16e7281be7a449
+    sudo add-apt-repository 'deb http://dl.hhvm.com/ubuntu trusty main'
+    sudo apt-get update
+    sudo apt-get install hhvm
+    sudo update-rc.d hhvm defaults
+    sudo /usr/share/hhvm/install_fastcgi.sh
+    sudo service hhvm restart
+    sudo service nginx restart
+@endtask
+
+@task('composer', ['on' => ['web-1', 'web-2'], 'parallel' => true])
+    curl -sS https://getcomposer.org/installer | sudo php -- --install-dir=/usr/bin --filename=composer
 @endtask
